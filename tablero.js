802b87fc9abfcb22c3affd7ac486a2428fdabb29
MasterxDual <agustinbram@gmail.com>
2022-02-01 18:48:04 -0300

    Creando la figura del ahorcado con Canvas v1.0

--- tablero.js
+++ tablero.js
@@ -3,6 +3,27 @@ function tableroJuego() {
     var pincel = pantalla.getContext("2d");
     pincel.fillStyle = "#F3F5FC";
     pincel.fillRect(100, 0, 1200, 800);
+    pincel.fill();
 }
-
 tableroJuego();
+function horca() {
+    var pantalla = document.querySelector("canvas");
+    var pincel = pantalla.getContext("2d");
+    pincel.strokeStyle = "#0A3871";
+    pincel.lineWidth = 6;
+    pincel.beginPath();
+    pincel.moveTo(506, 503);
+    pincel.lineTo(800, 503);
+    pincel.stroke();
+    pincel.moveTo(590, 503);
+    pincel.lineTo(590, 143);
+    pincel.stroke();
+    pincel.moveTo(590, 146);
+    pincel.lineTo(760, 146);
+    pincel.stroke();
+    pincel.moveTo(757, 146);
+    pincel.lineTo(757, 205);
+    pincel.stroke();
+    
+}
+horca();
