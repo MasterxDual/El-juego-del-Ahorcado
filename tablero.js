b1e3b9f05e6a4c02663d9b4489511543fe4dee5f
MasterxDual <agustinbram@gmail.com>
2022-02-04 12:41:58 -0300

    Terminando de dibujar el muñeco y agregando guiones bajos

--- tablero.js
+++ tablero.js
@@ -6,20 +6,22 @@ function tableroJuego() {
     pincel.fill();
 }
 tableroJuego();
+
 function horca() {
     var pantalla = document.querySelector("canvas");
     var pincel = pantalla.getContext("2d");
     pincel.strokeStyle = "#0A3871";
-    pincel.lineWidth = 6;
+    pincel.lineWidth = 5;
     pincel.beginPath();
     pincel.moveTo(506, 503);
     pincel.lineTo(800, 503);
     pincel.stroke();
+    pincel.lineWidth = 4.5;
     pincel.moveTo(590, 503);
     pincel.lineTo(590, 143);
     pincel.stroke();
-    pincel.moveTo(590, 146);
-    pincel.lineTo(760, 146);
+    pincel.moveTo(588, 145);
+    pincel.lineTo(759, 145);
     pincel.stroke();
     pincel.moveTo(757, 146);
     pincel.lineTo(757, 205);
@@ -41,8 +43,32 @@ function horca() {
     pincel.moveTo(759, 418);
     pincel.lineTo(725, 475);
     pincel.stroke();
-    pincel.moveTo(759, 418);
-    pincel.lineTo(793, 475);
+    pincel.moveTo(760, 418);
+    pincel.lineTo(796, 475);
     pincel.stroke();
 }
 horca();
+
+function guionesPalabras() {
+    var pantalla = document.querySelector("canvas");
+    var pincel = pantalla.getContext("2d");
+    pincel.strokeStyle = "#0A3871";
+    pincel.lineWidth = 4;
+    pincel.beginPath();
+    pincel.moveTo(420, 711);
+    pincel.lineTo(500, 711);
+    pincel.stroke();
+    pincel.moveTo(516, 711);
+    pincel.lineTo(596, 711);
+    pincel.stroke();
+    pincel.moveTo(612, 711);
+    pincel.lineTo(692, 711);
+    pincel.stroke();
+    pincel.moveTo(708, 711);
+    pincel.lineTo(788, 711);
+    pincel.stroke();
+    pincel.moveTo(804, 711);
+    pincel.lineTo(884, 711);
+    pincel.stroke();
+}
+guionesPalabras();
