7204a4efa80bbef4130cf8cc0507859950c73b88
MasterxDual <agustinbram@gmail.com>
2022-02-04 17:50:54 -0300

    Dibujando letras ganaste y perdiste

--- tablero.js
+++ tablero.js
@@ -72,3 +72,25 @@ function guionesPalabras() {
     pincel.stroke();
 }
 guionesPalabras();
+
+function letrasGanaste() {
+    var pantalla = document.querySelector("canvas");
+    var pincel = pantalla.getContext("2d");
+    var texto = "Ganaste, felicidades!";
+    pincel.beginPath();
+    pincel.font = "italic 30px Arial";
+    pincel.fillStyle = "green";
+    pincel.fillText(texto, 870, 285);
+}
+letrasGanaste();
+
+function letrasPerdiste() {
+    var pantalla = document.querySelector("canvas");
+    var pincel = pantalla.getContext("2d");
+    var texto = "Fin del juego!";
+    pincel.beginPath();
+    pincel.font = "italic 30px Arial";
+    pincel.fillStyle = "red";
+    pincel.fillText(texto, 870, 285);
+}
+letrasPerdiste();
